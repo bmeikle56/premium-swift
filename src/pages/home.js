b50a0d3b67c59aaddd350cbe9fbe0f14dc350fae
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-17 15:42:38 -0400

    Add ideas

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,18 @@ import { motion } from 'framer-motion'
 import { useState } from 'react'
 import Loading from '../cmp/Loading'
 
+let palette = {
+  white: 'rgb(0,0,0)',
+  pink: 'rgb(0,0,0)',
+  cyan: 'rgb(0,0,0)',
+  fadedBlue: 'rgb(0,0,0)',
+  deepBlue: 'rgb(0,0,0)',
+  green: 'rgb(0,0,0)',
+  orange: 'rgb(0,0,0)',
+  purple: 'rgb(0,0,0)',
+  comment: 'rgb(0,0,0)',
+}
+
 function Home() {
   const [isLoading, setLoading] = useState(true)
 
@@ -19,5 +31,24 @@ function Home() {
     )
   }
 }
+
+/*
+func login(username: String in {#"^(?=.*\d)[A-Za-z\d]{8,}$"#}, password: String) async -> Bool {
+    /// request the server validate the username and password...
+}
+
+----------------
+
+struct Card {
+    let suit: String
+    let rank: String
+}
+
+func value(
+    for card: Card in {suit: {"h", "s", "d", "c"}, rank: [0, 12]}
+) -> Int {
+    /// return the value of the Card...
+}
+*/
 
 export default Home
