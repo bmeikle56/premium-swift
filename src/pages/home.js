c55b731ea3381960e726f2fcea2275c3a2a5176e
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-08-17 16:18:11 -0400

    Complete first example

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,13 +5,14 @@ import Loading from '../cmp/Loading'
 let palette = {
   white: 'rgb(255,255,255)',
   pink: 'rgba(186, 2, 100, 1)',
-  cyan: 'rgba(1, 205, 236, 1)',
-  fadedBlue: 'rgba(51, 122, 167, 1)',
-  deepBlue: 'rgba(0, 112, 182, 1)',
+  cyan: 'rgba(48, 182, 222, 1)',
+  fadedBlue: 'rgba(58, 139, 189, 1)',
+  deepBlue: 'rgba(3, 140, 225, 1)',
   green: 'rgba(95, 227, 83, 1)',
   orange: 'rgba(233, 70, 0, 1)',
   purple: 'rgba(126, 89, 228, 1)',
   comment: 'rgba(0, 166, 3, 1)',
+  darkComment: 'rgba(1, 81, 2, 1)',
 }
 
 function Home() {
@@ -27,6 +28,37 @@ function Home() {
       transition={{ delay: 1.2, duration: 2.0 }}
       >
         <div style={{background: 'rgb(13,13,13)', padding: 20, borderRadius: 20, display: 'flex', flexDirection: 'column', gap: 4}}>
+          <div style={{display: 'flex'}}>
+            <pre style={{color: palette.pink, display: 'inline', padding: 0, margin: 0}}>struct</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.cyan, display: 'inline', padding: 0, margin: 0}}>Card</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{'{'}</pre>
+          </div>
+          <div style={{display: 'flex'}}>
+            <div style={{width: 30, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.pink, display: 'inline', padding: 0, margin: 0}}>{'let'}</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.fadedBlue, display: 'inline', padding: 0, margin: 0}}>{'suit'}</pre>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{':'}</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.deepBlue, display: 'inline', padding: 0, margin: 0}}>{'String'}</pre>
+          </div>
+          <div style={{display: 'flex'}}>
+            <div style={{width: 30, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.pink, display: 'inline', padding: 0, margin: 0}}>{'let'}</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.fadedBlue, display: 'inline', padding: 0, margin: 0}}>{'rank'}</pre>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{':'}</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.deepBlue, display: 'inline', padding: 0, margin: 0}}>{'String'}</pre>
+          </div>
+          <div style={{display: 'flex'}}>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{'}'}</pre>
+          </div>
+          <div style={{display: 'flex'}}>
+            <div style={{height: 24, padding: 0, margin: 0}}/>
+          </div>
           <div style={{display: 'flex'}}>
             <pre style={{color: palette.pink, display: 'inline', padding: 0, margin: 0}}>func</pre>
             <div style={{width: 8, padding: 0, margin: 0}}/>
@@ -61,8 +93,24 @@ function Home() {
             <pre style={{color: palette.purple, display: 'inline', padding: 0, margin: 0}}>{'0'}</pre>
             <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{','}</pre>
             <div style={{width: 8, padding: 0, margin: 0}}/>
-            <pre style={{color: palette.purple, display: 'inline', padding: 0, margin: 0}}>{'12'}</pre>
-            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{']'}</pre>
+            <pre style={{color: palette.purple, display: 'inline', padding: 0, margin: 0}}>{'13'}</pre>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{')'}</pre>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{'}'}</pre>
+          </div>
+          <div style={{display: 'flex'}}>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{') ->'}</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.deepBlue, display: 'inline', padding: 0, margin: 0}}>Int</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{'{'}</pre>
+          </div>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <div style={{width: 30, padding: 0, margin: 0}}/>
+            <pre style={{color: palette.darkComment, display: 'inline', padding: 0, margin: 0}}>{'///'}</pre>
+            <div style={{width: 8, padding: 0, margin: 0}}/>
+            <p style={{color: palette.comment, display: 'inline', padding: 0, margin: 0, fontSize: 12}}>{'return the value of the Card...'}</p>
+          </div>
+          <div style={{display: 'flex'}}>
             <pre style={{color: palette.white, display: 'inline', padding: 0, margin: 0}}>{'}'}</pre>
           </div>
         </div>
@@ -74,19 +122,6 @@ function Home() {
 /*
 func login(username: String in {#"^(?=.*\d)[A-Za-z\d]{8,}$"#}, password: String) async -> Bool {
     /// request the server validate the username and password...
-}
-
-----------------
-
-struct Card {
-    let suit: String
-    let rank: String
-}
-
-func value(
-    for card: Card in {suit: {"h", "s", "d", "c"}, rank: [0, 12]}
-) -> Int {
-    /// return the value of the Card...
 }
 */
 
