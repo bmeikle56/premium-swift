1dba0eccf6320550c352d16f24d59fc9016d6b49
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2026-02-26 22:12:05 -0500

    support cmp draft

--- cmp/ui/support.tsx
+++ cmp/ui/support.tsx
@@ -5,11 +5,12 @@ import {
 
 export default function Support() {
   return (
-    <div style={{display: 'flex', padding: 12, borderRadius: 8, background: bg1, height: 'fit-content', width: '80vw', maxWidth: 400}}>
-      <p style={{color: txt1}}>
-        {"Email me at "}
-      </p>
-      <a href="mailto:bameeks56@gmail.com"/>
+    <div style={{display: 'flex', flexDirection: 'column', padding: 12, borderRadius: 8, background: bg1, height: 'fit-content', width: '80vw', maxWidth: 400}}>
+      <a href="mailto:bameeks56@gmail.com">
+        <p style={{color: txt1}}>
+          {"Email me"}
+        </p>
+      </a>
       <p style={{color: txt1}}>
         {" for comments, feature recommendations, or if you were able to land a job preparing with this site!"}
       </p>
