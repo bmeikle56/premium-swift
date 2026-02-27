72f244092c280a54817f759ae358b1c60d2d6ce6
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2026-02-26 23:53:44 -0500

    coming soon tag

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -4,16 +4,13 @@ import Title from "@/cmp/ui/title";
 import PSButton from "@/cmp/ui/psbutton";
 import PSParagraph from "@/cmp/ui/psparagraph";
 import Wallpaper from "@/cmp/ui/wallpaper";
+import PSPre from "@/cmp/ui/pspre";
 
 export default function Page() {
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: 40, height: '100vh'}}>
       <Wallpaper/>
-      <Title/>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <pre></pre>
-      </div>
-      <PSButton txt={'Get Started'} route={'study'}/>
+      <PSPre txt="Coming soon!"/>
     </div>
   );
 }
