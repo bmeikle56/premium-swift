04cb4394c4036ebfb6241783c499e2e6384ced89
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2026-02-27 22:19:37 -0500

    btn subtxt add

--- cmp/nav/navbox.tsx
+++ cmp/nav/navbox.tsx
@@ -1,13 +1,13 @@
 import { CSSProperties } from "react";
 import PSButton from "../ui/psbutton";
 
-const sections: string[] = [
-  'Swift',
-  'SwiftUI',
-  'SDK',
-  'Objective-C',
-  'UIKit',
-  'Architecture'
+const sections: [string, string][] = [
+  ['Swift', '30 minutes'],
+  ['SwiftUI', '30 minutes'],
+  ['SDK', '30 minutes'],
+  ['Objective-C', '30 minutes'],
+  ['UIKit', '30 minutes'],
+  ['Architecture', '30 minutes']
 ]
 
 export default function Navbox() {
@@ -20,7 +20,7 @@ export default function Navbox() {
 
   return (
     <div style={style}>
-      {[...sections].map((txt, idx) => <PSButton txt={txt} key={idx} route={`/study/${txt.toLowerCase()}`}/>)}
+      {[...sections].map((tuple, idx) => <PSButton key={idx} txt={tuple[0]} subtxt={tuple[1]} route={`/study/${tuple[0].toLowerCase()}`}/>)}
     </div>
   )
 }
--- cmp/ui/psbutton.tsx
+++ cmp/ui/psbutton.tsx
@@ -2,17 +2,20 @@ import { CSSProperties } from "react";
 import { 
   bg1,
   txt1,
+  txt2,
   pur1
 } from "../cst/constants"
 
 import Link from "next/link";
+import { sub } from "framer-motion/client";
 
 export default function PSButton(
-  { txt, route }: { txt: string, route: string }
+  { txt, subtxt, route }: { txt: string, subtxt?: string, route: string }
 ) {
   const style: CSSProperties = {
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
     padding: 12,
     background: bg1,
     boxShadow: `0 0 4px ${pur1}, 0 0 8px ${pur1}`,
@@ -24,6 +27,7 @@ export default function PSButton(
     <Link href={route} passHref>
       <div style={style}>
         <pre style={{ color: txt1, margin: 0 }}>{txt}</pre>
+        {subtxt && <pre style={{ color: txt2, margin: 0, fontSize: 10, paddingTop: 6 }}>{subtxt}</pre>}
       </div>
     </Link>
   );
